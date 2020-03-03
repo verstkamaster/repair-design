9bdd0d7f75321cf7b5a767ef646d1b720212572b
Evgeniy <pchelka7979@gmail.com>
2020-03-03 18:56:10 +0300

    mucheniya s gulp udalis'

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
-
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
 gulp.task('hello', function(done) {
    console.log('Привет мир!');
@@ -8,11 +9,20 @@ gulp.task('hello', function(done) {
 });
 
 // Static server
-gulp.task('browser-sync', function() {
+gulp.task('server', function() {
    browserSync.init({
        server: {
            baseDir: "./"
        }
    });
-   gulp.watch("./*.html").on('change', browserSync.reload);
 });
+gulp.watch("./*.html").on('change', browserSync.reload);
+//  Minify CSS
+gulp.task('mincss', function () {
+   return gulp.src('./*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./'));
+});
+
+// gulp.watch("./*.css");
