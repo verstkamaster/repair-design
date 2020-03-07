60a22e02b89b06a6beef01e5bac0021f59f5ce87
Evgeniy <pchelka7979@gmail.com>
2020-03-08 02:04:46 +0300

    8-e marta, a server ne rabotaet)))

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,26 @@
-const {src, dest, watch} = require('gulp');
-const browserSync = require('browser-sync');
-//подключаем cssmin
-const cssmin = require('gulp-cssmin');
-const rename = require('gulp-rename');
-// подключаем gulp-sass
-const sass = require('gulp-sass');
-//autoprefixer
-const autoprefixer = require('gulp-autoprefixer');
-// Static server
+const { src, dest, watch } = require("gulp");
+const browserSync = require("browser-sync").create();
+// const cssmin = require("gulp-cssmin");
+// const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+// const autoprefixer = require("gulp-autoprefixer");
+
 function bs() {
-   browserSync.init({
-       server: {
-           baseDir: "./"
-       }
-   });
-   };
- // используем autoprefixer
-function serveSass() { 
-   return src("./sass/*.+(sass|scss)").on(error, sass.logError) 
-       .pipe(sass()) 
-       .pipe(autoprefixer({
-        cascade: false
-       }))
-       .pipe(dest('./css')) 
-       .pipe(browserSync.stream());
-      };
+  serveSass();
+  browserSync.init({
+    server: {
+      baseDir: "./src/"
+    }
+  });
+  watch("./src/*.html").on("change", browserSync.reload);
+  watch("./src/sass/**/*.sass)", serveSass);
+  // watch("./src/js/*.js").on("change", browserSync.reload);
+}
 
-watch("./*.html").on('change', browserSync.reload);
-watch("./sass/*.+(sass|scss)", serveSass);
-watch("./*.js/*.js").on('change', browserSync.reload);
-exports.serve = bs;      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//  Minify CSS
-// function mincss() {
-//         .src('./css/*.css')
-//         .pipe(cssmin())
-//         .pipe(rename({suffix: '.min'}))
-//         .pipe(dest('./css'));
-//         };
-
-// используем GULP-SASS
-// gulp.task('sass', function(done){
-//      gulp.src('./sass/**/*.scss')
-//      .pipe(sass().on('error', sass.logError)) 
-//      // используем gulp-sass
-//      .pipe(gulp.dest('./css'))
-//      done();
-// });
+function serveSass() {
+  return src("./src/sass/*.sass")
+    .pipe(sass())
+    .pipe(dest("./src/css"))
+    .pipe(browserSync.stream());
+}
+exports.serve = bs;
